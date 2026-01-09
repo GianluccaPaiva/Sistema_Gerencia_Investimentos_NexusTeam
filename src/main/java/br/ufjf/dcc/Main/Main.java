09f33298b1a89bbe8d215f73280a53e9dc22dca2
Gianlucca Paiva <gianluccapaiva@gmail.com>
2026-01-09 09:04:23 -0300

    rename Main.java and update package structure in pom.xml

--- src/main/java/br/ufjf/dcc/Main/Main.java
+++ src/main/java/br/ufjf/dcc/Main/Main.java
@@ -1,4 +1,4 @@
-package br.ufjf.dcc;
+package br.ufjf.dcc.Main;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
