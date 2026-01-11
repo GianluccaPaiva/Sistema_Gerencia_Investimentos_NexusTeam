34967bb1ac9c72dae017711abcdd1362a1369503
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2026-01-11 18:47:40 -0300

    Refactor ItemCarteira class to use float for quantity and add methods for managing purchases and sales

--- src/main/java/br/ufjf/dcc/Carteira/ItemCarteira.java
+++ src/main/java/br/ufjf/dcc/Carteira/ItemCarteira.java
@@ -1,17 +1,44 @@
 package br.ufjf.dcc.Carteira;
 import br.ufjf.dcc.Ativos.Ativos;
 import br.ufjf.dcc.Ativos.Interfaces.Internacional;
-import br.ufjf.dcc.Ativos.Stocks;
 
 public class ItemCarteira{
     private Ativos ativo;
-    private int qtd;
+    private float qtd;
     private double valorPagoTotal;
 
-
-    public ItemCarteira(Ativos ativo, int qtd, double precoPagoNaCompra){
+    public ItemCarteira(Ativos ativo, float qtd, double precoPagoNaCompra){
         this.ativo = ativo;
         this.qtd = qtd;
         this.valorPagoTotal = precoPagoNaCompra * qtd;
     }
+
+    public double getValorAtualTotal(){
+        double precoEmReais;
+        if(this.ativo instanceof Internacional){
+            precoEmReais = this.ativo.getPreco() * Internacional.COTACAO_DOLAR;
+        } else {
+            precoEmReais = this.ativo.getPreco();
+        }
+        return precoEmReais * this.qtd;
+    }
+
+    public void adicionarCompra(float novaQtd, double precoPagoNaCompra){
+        if (novaQtd > 0) {
+            this.qtd += novaQtd;
+            this.valorPagoTotal += (precoPagoNaCompra * novaQtd);
+        }
+    }
+
+    public void removerVenda(float qtdVendida){
+        if (qtdVendida > 0 && qtdVendida <= this.qtd) {
+            this.qtd -= qtdVendida;
+        }
+    }
+
+    public Ativos getAtivo(){return this.ativo;}
+    public float getQtd(){return this.qtd;}
+    public double getValorPagoTotal(){return this.valorPagoTotal;}
+    public double getLucroPrejuizo(){return getValorAtualTotal() - this.valorPagoTotal;}
+
 }
