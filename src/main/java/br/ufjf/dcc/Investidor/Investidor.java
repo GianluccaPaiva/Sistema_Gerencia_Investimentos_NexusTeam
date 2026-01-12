8902987505c090a4a012b9a7755d14e75a181b04
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2026-01-12 00:23:22 -0300

    Implement Investidor class with methods for managing investments and displaying portfolio

---
+++ src/main/java/br/ufjf/dcc/Investidor/Investidor.java
@@ -0,0 +1,67 @@
+package br.ufjf.dcc.Investidor;
+import br.ufjf.dcc.Ativos.Ativos;
+import br.ufjf.dcc.Carteira.Carteira;
+
+public abstract class Investidor {
+    private String nome, id, telefone, dataNascimento;
+    private Endereco endereco;
+    private double patrimonio;
+    private Carteira carteira;
+
+    public Investidor(String nome, String id, String telefone, String dataNascimento, Endereco endereco, double patrimonio) {
+        this.nome = nome;
+        this.id = id;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+        this.patrimonio = (patrimonio >= 0) ? patrimonio : 0;
+        this.carteira = new Carteira();
+    }
+    public void comprar(Ativos ativo, float qtd, double preco) {
+        if(podeComprar(ativo)){
+            carteira.addAtivo(ativo, qtd, preco);
+            System.out.println("Compra realizada com sucesso!");
+        }
+        else{
+            System.out.println("Compra não autorizada para este tipo de investidor.");
+        }
+    }
+    public void vender(Ativos ativo, float qtd){
+        carteira.removerAtivo(ativo.getTicker(), qtd);
+        System.out.println("Venda realizada com sucesso!");
+    }
+
+    public void exibirCarteira(){
+        carteira.exibirCarteira();
+    }
+
+    public abstract boolean podeComprar(Ativos ativo);
+
+    @Override
+    public String toString() {
+        return String.format("Nome: %-20s | ID: %-14s | Patrimônio: R$ %.2f", this.nome, this.id, this.patrimonio);
+    }
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+
+    public String getTelefone() {return telefone;}
+    public void setTelefone(String telefone) {this.telefone = telefone;}
+
+    public String getDataNascimento() {return dataNascimento;}
+    public void setDataNascimento(String dataNascimento) {this.dataNascimento = dataNascimento;}
+
+    public Endereco getEndereco() {return endereco;}
+
+    public double getPatrimonio() {return patrimonio;}
+    public void setPatrimonio(double patrimonio) {
+        if(patrimonio >= 0){
+            this.patrimonio = patrimonio;
+        }
+    }
+
+    public Carteira getCarteira() {return this.carteira;}
+}
